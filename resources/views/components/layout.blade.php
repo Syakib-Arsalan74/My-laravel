c74e29e6806ae9e5efce0b78322a28176f925509
Syakib-Arsalan74 <Syakibgithub@gmail.com>
2025-01-13 03:32:45 +0700

    init file

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title></title>
+</head>
+
+<body class="h-full">
+<div class="min-h-full">
+  <x-navbar></x-navbar>
+  <x-header>{{ $title }}</x-header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      {{ $slot }}
+    </div>
+  </main>
+</div>
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+  <x-slot:title>{{ $title }}</x-slot:title>
+  <h1>Hallo syakib</h1>
+</x-layout>
