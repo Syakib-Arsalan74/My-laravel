c63e57fec141659a22a7898e0cb2d1b895b171f4
Syakib-Arsalan74 <Syakibgithub@gmail.com>
2025-01-14 06:46:55 +0700

    memperbaiki dan menambah migrate

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,11 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
-  @foreach
+  
   <article>
     <div>
-      <a href="/post">
       <h2>{{$post["judul"]}}</h2>
-    </a>
     </div>
     <p>{{$post["article"]}}</p>
   </article>
-  @endforeach
+  
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +12,10 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact', ["title" => "Contact Me"]);
 });
-Route::get('/blog', function () {
+Route::get('/blog', function ($slug) {
+    $post = Post::find($slug);
     return view('blog', ["title" => "Blog"]);
+});
+Route::get('/post', function () {
+    return view('blog', ["title" => "Single Post"]);
 });
